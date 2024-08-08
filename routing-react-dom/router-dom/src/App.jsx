71f4db409eb7b396771e212bc6a26b250d7ada1a
Sivadheeraj5356 <sivadheerajethamsetti@gmail.com>
2024-08-09 00:06:03 +0530

    commit

--- routing-react-dom/router-dom/src/App.jsx
+++ routing-react-dom/router-dom/src/App.jsx
@@ -10,8 +10,14 @@ function App() {
 
   return (
     <div>
-    <div style={{backgroundColor:"black", color:"white" , margin:"0px", fontFamily:"sans-serif"}}>
-    <center> <h1>The Navbar of the page</h1> </center>
+    <div style={{backgroundColor:"black", color:"white" , margin:"0px", fontFamily:"sans-serif" , display:"flex" , justifyContent:"space-between"}}>
+    <h1>The Navbar of the page</h1>
+     <button style={{backgroundColor:"white" , height:"40px" , marginTop:"20px" , border:"0" , borderRadius:"10px" , fontSize:"18px"}} onClick={()=>{
+      window.location.href ='/dashboard'
+     }}>Dashboard</button>
+     <button style={{backgroundColor:"white" , height:"40px", width:"100px" , marginTop:"20px" , border:"0" , borderRadius:"10px" , fontSize:"18px" , marginRight:"600px"}} onClick={()=>{
+      window.location.href = "/"
+     }}>Landing</button> 
     </div>
 <BrowserRouter>
 <Routes>
