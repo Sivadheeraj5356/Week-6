80c0d9540cc7ea3c242ca2bc6c3983cc141269dc
Sivadheeraj5356 <sivadheerajethamsetti@gmail.com>
2024-08-09 00:54:07 +0530

    added useNavitage hook to use routing in react js

--- routing-react-dom/router-dom/src/App.jsx
+++ routing-react-dom/router-dom/src/App.jsx
@@ -1,25 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
 import './App.css'
 import { Dashboard } from './components/dashboard'
 import { Landing } from './components/landing'
 
 function App() {
-
   return (
     <div>
-    <div style={{backgroundColor:"black", color:"white" , margin:"0px", fontFamily:"sans-serif" , display:"flex" , justifyContent:"space-between"}}>
-    <h1>The Navbar of the page</h1>
-     <button style={{backgroundColor:"white" , height:"40px" , marginTop:"20px" , border:"0" , borderRadius:"10px" , fontSize:"18px"}} onClick={()=>{
-      window.location.href ='/dashboard'
-     }}>Dashboard</button>
-     <button style={{backgroundColor:"white" , height:"40px", width:"100px" , marginTop:"20px" , border:"0" , borderRadius:"10px" , fontSize:"18px" , marginRight:"600px"}} onClick={()=>{
-      window.location.href = "/"
-     }}>Landing</button> 
-    </div>
 <BrowserRouter>
+<Appbar></Appbar>
 <Routes>
   <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
   <Route path="/" element={<Landing></Landing>}></Route>
@@ -27,6 +18,21 @@ function App() {
 </BrowserRouter>
     </div>
   )
+}
+function Appbar() {
+  const navitage = useNavigate();
+
+  return <div>
+  <div style={{backgroundColor:"black", color:"white" , margin:"0px", fontFamily:"sans-serif" , display:"flex" , justifyContent:"space-between"}}>
+    <h1>The Navbar of the page</h1>
+     <button style={{backgroundColor:"white" , height:"40px" , marginTop:"20px" , border:"0" , borderRadius:"10px" , fontSize:"18px"}} onClick={()=>{
+   navitage("/dashboard")
+}}>Dashboard</button>
+     <button style={{backgroundColor:"white" , height:"40px", width:"100px" , marginTop:"20px" , border:"0" , borderRadius:"10px" , fontSize:"18px" , marginRight:"600px"}} onClick={()=>{
+      navitage("/")
+}}>Landing</button> 
+  </div>
+  </div>
 }
 
 export default App
