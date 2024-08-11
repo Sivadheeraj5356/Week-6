e746d59a91da58265c00d5b951e874480feff0a6
Sivadheeraj5356 <sivadheerajethamsetti@gmail.com>
2024-08-11 22:54:56 +0530

    recoil selector and atoms in recoil

--- routing-react-dom/router-dom/src/App.jsx
+++ routing-react-dom/router-dom/src/App.jsx
@@ -1,21 +1,45 @@
 import { useState } from "react";
 import { useContext } from "react";
-import { useRecoilState, useRecoilValue , RecoilRoot } from "recoil";
-import { countAtom } from "./store/atoms/count";
+import { useRecoilState, useRecoilValue , RecoilRoot, useSetRecoilState } from "recoil";
+import { countAtom, evenSelector } from "./store/atoms/count";
 function App(){
    return (
     <div>
-    <RecoilRoot>
-     <Count></Count>
+     <RecoilRoot>
+     <AddTodo></AddTodo>
      </RecoilRoot>
     </div>
    )
+}
+function AddTodo(){
+  const[title,setTitle] = useState("")
+  const[description,setDescription] = useState("")
+  const[timing,setTiming] = useState("")
+  function HandleTasks(){
+
+  }
+  return <div>
+    <input type="text" placeholder="Title" onChange={(e)=>{
+      setTitle(e.target.value)
+    }} /> <br />
+    <input type="text" placeholder="Description" onChange={(e)=>{
+      setDescription(e.target.value)
+    }} /> <br />
+    <input type="text" placeholder="Timing" onChange={(e)=>{
+      setTiming(e.target.value)
+    }} /> <br /> <br />
+    <button onClick={HandleTasks}>Add Tasks</button> <br />
+   
+  </div>
 }
 function Count(){
   return <div>
 
     <CountRenderer></CountRenderer>
    <Buttons ></Buttons>
+   <OddOrEven></OddOrEven>
+   <EvenCountRenderer></EvenCountRenderer>
+   
   </div>
 }
 function CountRenderer(){
@@ -26,15 +50,37 @@ function CountRenderer(){
 }
 
 function Buttons(){
-  const [count, setCount] = useRecoilState(countAtom)
+  // const [count, setCount] = useRecoilState(countAtom)
+  const setCount = useSetRecoilState(countAtom)
 
   return <div>
      <button onClick={()=>{
-      setCount(count +1)
+      setCount(count => count +1)
      }}> Increase</button>
      <button onClick={()=>{
-      setCount(count -1)
+      setCount(count => count-1)
      }}> Decrease</button>
   </div>
+}
+function OddOrEven(){
+  const count = useRecoilValue(countAtom)
+  if(count % 2 == 0){
+    return <div>
+      <h2>It is even</h2>
+    </div>
+  }
+  else{
+    return <div>
+      <h2>It is odd</h2>
+    </div>
+  }
+}
+function EvenCountRenderer(){
+  const isEven = useRecoilValue(evenSelector)
+  return(
+    <div>
+      {isEven ? "It is odd" : "It is even"}
+    </div>
+  )
 }
  export default App
--- routing-react-dom/router-dom/src/store/atoms/count.jsx
+++ routing-react-dom/router-dom/src/store/atoms/count.jsx
@@ -1,6 +1,14 @@
-import {atom} from "recoil"
+import {atom, selector} from "recoil"
 
 export const countAtom = atom({
     default: 0,
     key : "countAtom"
+})
+
+export const evenSelector = selector({
+    key : "evenSelector",
+    get : ({get})=>{
+   const count = get(countAtom)
+   return count % 2;
+    }
 })
